fbb44a988ee766ab6b230c7e8d886cd6fabd1308
joxCastro <101573273+joxCastro@users.noreply.github.com>
2023-10-03 16:15:11 -0300

    cambios

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const port = 80; // Puedes usar el puerto que desees
 
 // Configura la conexión a la base de datos MySQL
 const dbConfig = {
-  host: process.env.DB_HOST, /**/
-  user: process.env.DB_USER, /*empresa27.empresadns.net----tmcapaci_pagina2----lIdLiX&uCVqv----tmcapaci_pagina2*/
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'empresa27.empresadns.net', /*process.env.DB_HOST----process.env.DB_USER----process.env.DB_PASSWORD----process.env.DB_NAME*/
+  user: 'tmcapaci_pagina2', /*empresa27.empresadns.net----tmcapaci_pagina2----lIdLiX&uCVqv----tmcapaci_pagina2*/
+  password: 'lIdLiX&uCVqv',
+  database: 'tmcapaci_pagina2',
 };
 
 const db = mysql.createConnection(dbConfig);
